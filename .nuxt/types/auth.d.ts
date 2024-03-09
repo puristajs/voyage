6d8f94b5f5e3ab87b3342a45d6833b53750d2210
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2024-03-09 11:20:25 +0000

    chore: revert

--- .nuxt/types/auth.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '#auth' {
-  const getServerSession: typeof import('/Users/sebastianwessel/projekte/voyage/node_modules/@sidebase/nuxt-auth/dist/runtime/server/services').getServerSession
-  const getToken: typeof import('/Users/sebastianwessel/projekte/voyage/node_modules/@sidebase/nuxt-auth/dist/runtime/server/services').getToken
-  const NuxtAuthHandler: typeof import('/Users/sebastianwessel/projekte/voyage/node_modules/@sidebase/nuxt-auth/dist/runtime/server/services').NuxtAuthHandler
-
-}
